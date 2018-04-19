5664136ec9f8df20c1ad5c890fd5275fbe0ded90
Hieu <35733698+hieudoan9835@users.noreply.github.com>
2018-04-19 09:52:10 -0400

    Update CapturePicture.py

--- CapturePicture.py
+++ CapturePicture.py
@@ -1,18 +1,20 @@
 import cv2
 import time
+def take_picture:
+    cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
+    while(1):
+        cap.set(cv2.CAP_PROP_AUTOFOCUS, True)
+        cap.set(cv2.CAP_PROP_POS_MSEC, True)
+        ret, frame = cap.read()
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-while(1):
-    cap.set(cv2.CAP_PROP_AUTOFOCUS, True)
-    cap.set(cv2.CAP_PROP_POS_MSEC, True)
-    ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-
-    time.sleep(2) #set time for turn off the camera
-    out = cv2.imwrite('hello.jpg', frame)
-    break
-cap.release()
-cv2.destroyAllWindows()
+        time.sleep(2) #set time for turn off the camera
+        out = cv2.imwrite('hello.jpg', frame)
+        break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+    
 
 
