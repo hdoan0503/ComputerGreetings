327711a898cc7b0b84237a985cdede9f128d7987
Hieu <35733698+hieudoan9835@users.noreply.github.com>
2018-04-27 15:55:29 -0400

    Update CapturePicture.py

--- CapturePicture.py
+++ CapturePicture.py
@@ -4,12 +4,11 @@ def take_picture():
     cap = cv2.VideoCapture(0)
 
     while(1):
-        cap.set(cv2.CAP_PROP_AUTOFOCUS, True)
-        #cap.set(cv2.CAP_PROP_POS_MSEC, True)
         ret, frame = cap.read()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-
-        cv2.imshow('frame', rgb)
+        #rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        #cap.set(cv2.CAP_PROP_POS_MSEC, True)
+        cap.set(cv2.CAP_PROP_AUTOFOCUS, True)
+        cv2.imshow('frame', frame)
         out = cv2.imwrite('hello.jpg', frame)
         #img = cv2.imread('hello.jpg',0)
         #cv2.imshow('image', img)
