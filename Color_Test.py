26fc71ed2e7766f0e999766d9ece3c04261b48d1
Hieu <35733698+hieudoan9835@users.noreply.github.com>
2018-05-01 15:41:22 -0400

    Update Color_Test.py

--- Color_Test.py
+++ Color_Test.py
@@ -32,34 +32,34 @@ def color_picker():
     b = rgb[2]
 
     #convert them to HSV
-    green = np.uint8([[[r,g,b]]])
-    color = cv2.cvtColor(green,cv2.COLOR_RGB2HSV)
+    np_color = np.uint8([[[r,g,b]]])
+    color = cv2.cvtColor(np_color,cv2.COLOR_RGB2HSV)
     #print(type(color))
-    #print(color)
+    print(color)
     # Takes in a list of 3 HSV values and sorts them based on their approximate color shade.
 
         # Red-> Orange
-    if(color[0,0,0] >= 0 and color[0,0,0] <= 25 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 50 and color[0,0,2] <=255):
+    if(color[0,0,0] >= 0 and color[0,0,0] <= 25 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 40 and color[0,0,2] <=255):
         print("red")
         color = 'red'
         # Yellow
-    elif(color[0,0,0] >= 25 and color[0,0,0] <= 40 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 50 and color[0,0,2] <=255):
+    elif(color[0,0,0] >= 25 and color[0,0,0] <= 40 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 40 and color[0,0,2] <=255):
         print("yellow")
         color = 'yellow'
         # Green
-    elif(color[0,0,0] >= 40 and color[0,0,0] <= 85 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 50 and color[0,0,2] <=255):
+    elif(color[0,0,0] >= 40 and color[0,0,0] <= 85 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 40 and color[0,0,2] <=255):
         print("green")
         color = 'green'
         # Blue
-    elif(color[0,0,0] >= 85 and color[0,0,0] <= 120 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 50 and color[0,0,2] <=255):
+    elif(color[0,0,0] >= 85 and color[0,0,0] <= 120 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 40 and color[0,0,2] <=255):
         print("blue")
         color = 'blue'
         #Pink->Purple
-    elif(color[0,0,0] >= 120 and color[0,0,0] <= 160 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 50 and color[0,0,2] <=255):
+    elif(color[0,0,0] >= 120 and color[0,0,0] <= 160 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 40 and color[0,0,2] <=255):
         print("pink")
         color = 'pink'
         # Black
-    elif(color[0,0,0] >= 0 and color[0,0,0] <= 180 and color[0,0,1] >= 40 and color[0,0,1] <=255 and color[0,0,2] >= 0 and color[0,0,2] <=30):
+    elif(color[0,0,0] >= 0 and color[0,0,0] <= 180 and color[0,0,1] >= 0 and color[0,0,1] <=255 and color[0,0,2] >= 0 and color[0,0,2] <=30):
         print("black")
         color = 'black'
     else:
@@ -67,6 +67,8 @@ def color_picker():
         color = 'No Color'
 
     return color
+
+
 
 
 
