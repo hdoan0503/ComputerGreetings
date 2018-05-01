48ce6e526424a560a253fa22f0d246ff890343e5
Hieu <35733698+hieudoan9835@users.noreply.github.com>
2018-05-01 15:39:23 -0400

    Update CapturePicture.py

--- CapturePicture.py
+++ CapturePicture.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 def take_picture():
     cap = cv2.VideoCapture(0)
 
@@ -12,7 +11,8 @@ def take_picture():
         out = cv2.imwrite('hello.jpg', frame)
         #img = cv2.imread('hello.jpg',0)
         #cv2.imshow('image', img)
-        time.sleep(2)
+        #wait 2 second (take the picture in 2 secs) before turn off the camera 
+        cv2.waitKey(2000)
         break
 
     cap.release()
